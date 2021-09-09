09dd5b39c30279caf1d5fa602f15ed2d119e04d6
oracle727 <daisuke.morita727@gmail.com>
2021-09-09 11:38:32 +0000

    Intialcommit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,61 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+    redirect_to book_path(@book)
+    flash[:notice] = "Book was successfully created."
+    else
+    @books = Book.all
+    render :index
+
+    end
+
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    # if @book.save
+    # redirect_to  book_path(@book)
+  #   else
+  #   render :edit
+  # flash[:notice] = "Book was successfully updated."
+  #   end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
+     flash[:notice] =  "Book was successfully destroyed."
+    redirect_to books_path(@book.id)
+    else
+    render :index
+    end
+   
+  end
+
+  def update
+   @book = Book.find(params[:id])
+   if @book.save
+      flash[:notice] = "Book was successfully updated."
+   redirect_to book_path(@book.id)
+   else
+   render :index
+   end
+
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
