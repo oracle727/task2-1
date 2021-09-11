c24bb4216cd179a7859fbbfe456340cdd7a79468
oracle727 <daisuke.morita727@gmail.com>
2021-09-11 06:23:45 +0000

    Initialcommit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -39,16 +39,16 @@ class BooksController < ApplicationController
     else
     render :index
     end
-   
+
   end
 
   def update
    @book = Book.find(params[:id])
-   if @book.save
+   if @book.update (book_params)
       flash[:notice] = "Book was successfully updated."
    redirect_to book_path(@book.id)
    else
-   render :index
+   render :edit
    end
 
   end
